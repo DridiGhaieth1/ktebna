fab1b6d30a439d77156d37a531b99555772e159c
Tarek Taghouti <tarek.taghouti@altran.com>
2021-07-18 21:19:57 +0200

    fix duplicated component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -37,7 +37,6 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-import { BoardUserComponent } from './board-user/board-user.component';
 
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
