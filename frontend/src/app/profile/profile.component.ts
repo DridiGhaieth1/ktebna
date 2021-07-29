f8b0065596b6e66607493af3fc1eb982a188f8ae
Taghouti Tarek <taghoutitarek@gmail.com>
2021-07-29 15:48:36 +0100

    update profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
+import {User} from '../model/user';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,23 @@ import { TokenStorageService } from '../_services/token-storage.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  currentUser: any;
-
-  constructor(private token: TokenStorageService) { }
+  currentUser: User = {
+    id: 0,
+    email: '',
+    roles: [],
+    name: '',
+    phone: 0,
+    points: 0,
+    isAdmin: false,
+    username: ''
+  };
+  currentToken: any;
+  constructor(private token: TokenStorageService) {
+    this.currentToken = token;
+  }
 
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
+    this.currentToken = this.token.getToken();
   }
 }
