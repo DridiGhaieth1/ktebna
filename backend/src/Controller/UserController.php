e61fa8706f87a68d4f255aa70a24f14957efbc5e
Taghouti Tarek <taghoutitarek@gmail.com>
2021-07-29 12:16:02 +0100

    add get user by token

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -3,11 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class UserController extends ApiController
 {
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $storage;
+    /**
+     * @var JWTTokenManagerInterface
+     */
+    private $jwtManager;
+
+    public function __construct(TokenStorageInterface $storage, JWTTokenManagerInterface $jwtManager)
+    {
+        $this->storage = $storage;
+        $this->jwtManager = $jwtManager;
+    }
+
     /**
      * @param UserRepository $userRepository
      * @param int $id
@@ -22,6 +42,27 @@ class UserController extends ApiController
         $user = $userRepository->find($id);
         if (!$user) return $this->respondNotFound('User not found');
         return $this->json($user);
+    }
+
+    /**
+     * @param UserRepository $userRepository
+     * @return JsonResponse
+     * @Route("/api/users/token", name="users_get", methods={"GET"})
+     */
+    public function getUserByToken(UserRepository $userRepository): JsonResponse
+    {
+        try {
+            $decodedJwtToken = $this->jwtManager->decode($this->storage->getToken());
+        } catch (JWTDecodeFailureException $e) {
+            return $this->respondNotFound($e->getMessage());
+        }
+        if (is_array($decodedJwtToken)) {
+            $user = $userRepository->findOneBy(['email' => $decodedJwtToken['username']]);
+            if (!$user) return $this->respondNotFound('User not found');
+            return $this->json($user);
+        } else {
+            return $this->respondNotFound('User not found');
+        }
     }
 
 }
