23903aeccb943878713f89d0d4a4fa3fadc7fce4
Taghouti Tarek <taghoutitarek@gmail.com>
2021-07-29 18:26:49 +0100

    update profile

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -62,6 +65,42 @@ class UserController extends ApiController
             return $this->json($user);
         } else {
             return $this->respondNotFound('User not found');
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $userRepository
+     * @return JsonResponse
+     * @Route("/api/users/token", name="posts_put", methods={"PUT"})
+     * @throws Exception
+     */
+    public function updateUserByToken(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): JsonResponse
+    {
+        try {
+            try {
+                $decodedJwtToken = $this->jwtManager->decode($this->storage->getToken());
+            } catch (JWTDecodeFailureException $e) {
+                return $this->respondNotFound($e->getMessage());
+            }
+            if (is_array($decodedJwtToken)) {
+                $user = $userRepository->findOneBy(['email' => $decodedJwtToken['username']]);
+                if (!$user) return $this->respondNotFound('User not found');
+                $request = $this->transformJsonBody($request);
+                if (!$request || !$request->get('name') || !$request->request->get('email') || !$request->request->get('phone')) {
+                    throw new Exception("All fields are required");
+                }
+                $user->setName($request->get('name'));
+                $user->setEmail($request->get('email'));
+                $user->setPhone($request->get('phone'));
+                $entityManager->flush();
+                return $this->respondWithSuccess('User updated');
+            } else {
+                return $this->respondNotFound('User not found');
+            }
+        } catch (Exception $e) {
+            return $this->respondValidationError('Validate data');
         }
     }
 
